8561b81f964ef8dcf6f0606d95310b77e9b57b06
DragonflyStats <kobriendublin@gmail.com>
2016-10-06 21:10:59 +0100

    Update 00-InbuiltDataSets.py

--- 00-InbuiltDataSets.py
+++ 00-InbuiltDataSets.py
@@ -3,3 +3,14 @@ from bokeh.sampledata.iris import flowers
 # Try Out
 # - flowers.head(5)
 # - flowers.describe()
+
+###################################################
+
+## Downloading Sample Data
+
+# - Some of the Bokeh examples rely on sample data that is not included 
+# - in the Bokeh GitHub repository or released packages, due to their size. 
+# - Once Bokeh is installed, the sample data can be obtained by executing 
+# - the following command at a Bash or Windows prompt:
+
+# bokeh sampledata
