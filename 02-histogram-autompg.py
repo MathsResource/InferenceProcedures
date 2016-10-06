f8e480ad9d4902b9bfb0317239eea533ac69eb1a
DragonflyStats <kobriendublin@gmail.com>
2016-10-06 23:15:53 +0100

    Create 02-histogram-autompg.py

---
+++ 02-histogram-autompg.py
@@ -0,0 +1,10 @@
+from bokeh.charts import Histogram, output_file, show
+from bokeh.layouts import row
+from bokeh.sampledata.autompg import autompg as df
+
+hist = Histogram(df, values='mpg', title="Auto MPG Histogram", plot_width=400)
+hist2 = Histogram(df, values='mpg', label='cyl', color='cyl', legend='top_right',
+                  title="MPG Histogram by Cylinder Count", plot_width=400)
+
+output_file('hist.html')
+show(row(hist, hist2))
