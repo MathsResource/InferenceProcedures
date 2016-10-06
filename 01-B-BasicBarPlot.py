aa3cd0bbcf56e9ba338489a9bdb436f491643e03
DragonflyStats <kobriendublin@gmail.com>
2016-10-06 19:23:07 +0100

    Update 01-B-BasicBarPlot.py

--- 01-B-BasicBarPlot.py
+++ 01-B-BasicBarPlot.py
@@ -1,17 +1,18 @@
-
 # Preliminaries
 import pandas as pd
+
+# Import all the main commands that we will use here
 from bokeh.charts import Bar, output_file, show
 
 # Create A Simple Data Set
-dict = {'sec': {u'A': 10, u'B': 20}}
+dict = {'sec': {u'A': 8, u'B': 12, u'C': 15, u'D': 14}}
 df = pd.DataFrame(dict)
 df
 
 # Setup an output location
-output_file("bar.html")
+output_file("MyBasicBarPlot.html")
 
-# Render the Bar Plot
+# Render the Bar Plot - Very Basic to start off with
 p = Bar(df)
 
 # Present the Bar Plot
