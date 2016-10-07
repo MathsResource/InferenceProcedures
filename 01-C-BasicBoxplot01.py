4109717be3b848f127ea9d0cc03f13407ed6b50e
DragonflyStats <kobriendublin@gmail.com>
2016-10-07 23:52:21 +0100

    Update 01-C-BasicBoxplot01.py

--- 01-C-BasicBoxplot01.py
+++ 01-C-BasicBoxplot01.py
@@ -4,10 +4,10 @@ from bokeh.sampledata.autompg import autompg as df
 # origin = the source of the data that makes up the autompg dataset
 title = "MPG by Cylinders"
 
-
+# N.B. "label" and "values"
 box_plot = BoxPlot(df, label=['cyl'], values='mpg',title=title)
 
 #Notice that Outliers Are Present
-output_file("myFirstBasicBoxplot.html", title="01-C-boxplot_01.py example")
+output_file("myFirstBasicBoxplot.html", title="boxplot_01.py example")
 
 show(box_plot)
