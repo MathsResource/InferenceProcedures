28ab745a6779b1099b6ab6922c81cab95f064d44
DragonflyStats <kobriendublin@gmail.com>
2016-10-08 09:59:21 +0100

    Update ImportPalette.py

--- PalettesLegend/ImportPalette.py
+++ PalettesLegend/ImportPalette.py
@@ -1,3 +1,12 @@
+# Palettes
+
+# Palettes are sequences (lists or tuples) of RGB(A) hex strings that define a colormap 
+# and be can set as the palette attribute of all chart types from bokeh.charts and as 
+# the color attribute of many plot objects from bokeh.plotting. 
+# Bokeh offers many of the standard Brewer palettes, which can be imported 
+# from the bokeh.palettes module.
+
+###################################
 
 # Importing a Palette
 from bokeh.palettes import Spectral6
