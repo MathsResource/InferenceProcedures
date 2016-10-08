dfa4bdb480badcaa9ab7091a803a9f093b8fb769
DragonflyStats <kobriendublin@gmail.com>
2016-10-08 12:38:48 +0100

    Update DiamondsExample.py

--- Heatmaps/DiamondsExample.py
+++ Heatmaps/DiamondsExample.py
@@ -1,6 +1,7 @@
 # Diamonds
 import os
 import numpy as np
+import pandas as pd
 
 os.getcwd()
 
@@ -9,27 +10,46 @@ os.getcwd()
 # - Create a Two Way Frequency Table for Cut and Color
 
 
-import pandas as pd
 
 diamonds = pd.read_csv("diamonds.csv")
-
 diamonds.tail(5)
-
 diamonds.shape
-
-
 diamonds.describe()
-
-
 byCutColor = diamonds.groupby(["cut","color"])
-
-
 output = byCutColor["carat"].count()
-
-
 # byCutColor["carat"].count().unstack()
 
 len(output)
 
-# Remove Index
-# - http://stackoverflow.com/questions/20107570/removing-index-column-in-pandas
+###########################################################################################
+
+
+
+
+byCutColor["carat"].count().unstack()
+
+
+
+#################################################
+
+myData = byCutColor["carat"].count()
+
+myData2 = myData #  Two Copies - I want to show something off
+################################################
+type(myData)
+
+myData2.reset_index
+
+myData.index[1]
+
+#################################################
+
+collect=pd.DataFrame(columns=("V1","V2"))
+
+for i in range(5):
+    #print(myData.index[i])
+    Temp1 = myData.index[i][0]
+    Temp2 = myData.index[i][1]
+    AddRow=pd.Series([Temp1,Temp2],index=["V1","V2"])
+    print(AddRow)
+    collect = collect.append(AddRow,ignore_index=True)
